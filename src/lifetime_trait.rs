f09a16b9e02e89bdee34d5d4146604b6c3d87bbb
mibi <binhhm2009@gmail.com>
2022-11-02 12:33:08 +0700

    Done lifetime and trait exercise

--- src/lifetime_trait.rs
+++ src/lifetime_trait.rs
@@ -1,4 +1,105 @@
 /////////////////////////////////////
+// Bài 1
+// Sửa code để compile thành công liên quan tới vấn đề lifetime
+/////////////////////////////////////
+
+use std::io;
+fn main() {
+    let mut input: Vec<String>;
+    loop {
+        let mut input_text = String::new();
+        println!("Type instruction in the format Add <name> to <department>:");
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("failed to read from stdin");
+        let trimmed_text: String = input_text.trim().to_string();
+        input = trimmed_text.split(" ").map(String::from).collect();
+        if input.len() < 3 {
+            println!("Invalid format.");
+        } else {
+            if input[0] == "Add" && input[2] == "to" {
+                break;
+            } else {
+                println!("Invalid format.");
+            }
+        }
+    }
+    println!("{:?}", input);
+}
+
+/////////////////////////////////////
+// Bài 2
+// Implement trait
+// Thực hiên cargo test
+/////////////////////////////////////
+
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+impl AppendBar for String {
+    fn append_bar(self) -> Self {
+        self + "Bar"
+    }
+}
+
+fn main() {
+    let s: String = String::from("Foo");
+    let s = s.append_bar();
+    println!("s: {}", s);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_foo_bar() {
+        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
+    }
+
+    #[test]
+    fn is_bar_bar() {
+        assert_eq!(
+            String::from("").append_bar().append_bar(),
+            String::from("BarBar")
+        );
+    }
+}
+
+/////////////////////////////////////
+// Bài 3
+// Implement trait
+// Thực hiên cargo test
+/////////////////////////////////////
+
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_owned());
+        self
+    }
+}
+
+fn main() {}
+
+//TODO: Add your code here
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_vec_pop_eq_bar() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+}
+
+/////////////////////////////////////
 // Bài 4
 // Implement trait
 // Thực hiên cargo test
